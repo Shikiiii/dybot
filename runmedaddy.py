942e57ee11d22efe80a78ee578233a55e3a9872f
Shikii_ <33571437+Thegamesbg@users.noreply.github.com>
2019-08-04 18:03:18 +0300

    nani

---
+++ runmedaddy.py
@@ -0,0 +1,24 @@
+import discord
+from discord import Message, Guild
+from discord.ext.commands import Bot
+
+bot = Bot(command_prefix='!')
+bot.remove_command('help')
+
+server: Optional[Guild] = None
+
+@bot.event
+async def on_ready():
+    global server
+    server = bot.get_guild(448571905524498432)
+
+    await bot.change_presence(activity=discord.Game(name='with your feelings.'))
+    print('Ready!')
+
+@bot.event
+async def on_member_join(member):
+    if(bot.guild.id == 448571905524498432):
+        channel = discord.utils.get(server.channels, name="☆│lounge") 
+        await channel.send("Welcome {} to **e nightclub!** You’re the {}.".format(member.mention, server.member_count))
+
+bot.run(os.environ.get("token"))
