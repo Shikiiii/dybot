0bd0a91b02cabae119ddcdf2f91db250fe245250
Shikii_ <33571437+Thegamesbg@users.noreply.github.com>
2019-08-12 03:34:04 +0300

    lets try this (2)

--- coolbot.py
+++ coolbot.py
@@ -89,7 +89,7 @@ async def on_message(message: Message):
         embed2.set_image(url="https://media.giphy.com/media/j6HHIdWsJFuPTPfNlW/giphy.gif")
         await message.channel.send(embed=embed2)
         await message.channel.send(embed=embed1)
-    if(message.content == "!rules" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
+    elif(message.content == "!rules" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
         embed1 = discord.Embed(title="**༚ ✧˳⁺ __Server rules__ ⁺˳✧ ༚**", description="__``1``__ • Make sure to Follow **Discord TOS** and **Community Guidelines** (TOS:  ``https://discordapp.com/terms`` | Community Guidelines: ``https://discordapp.com/guidelines``). \r\n\r\n __``2``__ • Keep content in the appropriate channels. This includes only posting NSFW content in NSFW marked channels. \r\n\r\n __``3``__ • No Doxing / Do not leak someone's IP address / Do not share personal information of anyone. \r\n\r\n __``4``__ • Any type of serious harassment [Blackmailing, Hate speech, DM spamming members, etc..]. Will result in a ban. \r\n\r\n __``5``__ • No advertising (Including PM advertisement). \r\n\r\n __``6``__ • Do NOT tell people to kill themselves (even if it's a joke). \r\n\r\n __``7``__ • Do not impersonate other users. Impersonating our staff team or discord staff will result in a permanent ban. \r\n\r\n __``8``__ • Remember that this server is English only! Try to avoid using any other languages so everyone can understand each other and have fun. \r\n\r\n __``9``__ • Respect all staff and follow their instructions. Especially the owners ( dy & scopes ). \r\n\r\n __``10``__ • Do NOT set people up against each other or against STAFF. \r\n\r\n __``11``__ • Respect people's wishes considering revealing their age, face reveal etc. \r\n\r\n __``12``__ • Asking for nudes or for other information is ABSOLUTELY not accepted here and will result in a ban. \r\n\r\n **- Please contact <@495680416422821888> if you have any issues with Staff or the rules posted above.** \r\n\r\n **- Make sure to check <#567371465633169436> if you're new to discord or have any questions.** \r\n\r\n **- Thank you for joining e- nightclub! We hope you have a great stay here!**", color=0xFF93F0)
         embed2 = discord.Embed(color=0xFF93F0)
         embed2.set_image(url="https://media.giphy.com/media/ZXBS4ZfZKU1EbTXlL8/giphy.gif")
@@ -97,7 +97,7 @@ async def on_message(message: Message):
         await message.channel.send(embed=embed2)
         await message.channel.send(embed=embed3)
         await message.channel.send(embed=embed1)
-    if(message.content == "!faq" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
+    elif(message.content == "!faq" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
         embed1 = discord.Embed(title="*__**FAQ**:__*", description="__**How can I level up?**__ \r\n\r\n To level up you have to be active in any channel in the server, avoid spamming. Spamming won't level you up. \r\n\r\n __**Is there a way to get picture perms/embed links?**__ \r\n\r\n Yes there is a way to get these perms, when you reach **level 10+** you'll be able to post pictures or links in <#491366183447298068>. \r\n\r\n __**Someone is advertising in my DMS what do I do?**__ \r\n\r\n Dm a staff member and they'll ban them as soon as possible. \r\n\r\n __**Staff is abusing his perms, what do I do?**__ \r\n\r\n Dm <@303564745565536256> or <@495680416422821888>. \r\n\r\n __**Is there a NSFW channel?**__ \r\n\r\n Yes there is, to get access to the NSFW + shitpost channels you have to get the NSFW role. (can be found in <#491368164370677781>). \r\n\r\n __**Do you guys do giveaways and events?**__ \r\n\r\n Yes we do events and giveaways sometimes. (get the events + giveaway roles in <#491368164370677781> so you don't miss our giveaways and events!) \r\n\r\n __**I want to become a Staff member for e - nightclub , how can I apply?**__ \r\n\r\n You can apply in <#566420979069222912> by typing **!apply** there! Please do not annoy the owners to get Mod/Admin. \r\n\r\n __**I want to apply for a Partner Manager, how can I do that?**__ \r\n\r\n Dm dy#0777. \r\n\r\n __**Where can I shoutout my instagram , snapchat, etc..?**__ \r\n\r\n You can send your snapcode in <#558992750612054036>, for instagram go to <#558992817532436521>.", color=0xFF93F0)
         embed2 = discord.Embed(color=0xFF93F0)
         embed2.set_image(url="https://media.giphy.com/media/kH73TzZ51Lz0ocgRJR/giphy.gif")
@@ -105,7 +105,7 @@ async def on_message(message: Message):
         await message.channel.send(embed=embed2)
         await message.channel.send(embed=embed1)
         await message.channel.send(embed=embed3)
-    if(message.content == "!staff" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
+    elif(message.content == "!staff" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
         embed1 = discord.Embed(title="**__Staff Members:__**", description="☆ - Owners: dy <@495680416422821888> | scopes <@303564745565536256>. \r\n\r\n ☆ - Co Owners: Blitzzy <@322362178306965504> | Djimi <@322361798625853441> | Ave <@340478577906548737> | Ashton <@335445790808080385> | Blury <@315076865171783682> \r\n\r\n ☆ - Head Admins: N/A \r\n\r\n ☆ - Admins: Quenty <@243218955014111232> | Lil Akame <@464447422249304084> \r\n\r\n ☆ - Mods: Poppy <@454036738885681162> | Sammy <@541048817307353095>", color=0xFF93F0)
         embed2 = discord.Embed(color=0xFF93F0)
         embed2.set_image(url="https://media.giphy.com/media/Xy1debdAWrNLK3cnHk/giphy.gif")
@@ -113,23 +113,23 @@ async def on_message(message: Message):
         await message.channel.send(embed=embed2)
         await message.channel.send(embed=embed1)
         await message.channel.send(embed=embed3)
-    if(message.content == "!verification" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
+    elif(message.content == "!verification" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
         embed1 = discord.Embed(title="**༚ ✧˳⁺ Verification ⁺˳✧ ༚**", description="✧ - Post selfie in <#558991375681716256> with 'e- Nightclub and your discord username' on a piece of paper to get verified! \r\n\r\n ✧ - Verified role gives you access to <#558992153393496074> or <#558991846785679360> depends on your gender. \r\n\r\n ✧ - **NOTE:** You cannot use a photo that you took for another server. \r\n\r\n You can also Dm me or dm a staff member to role you verified if you don't want to post your face in <#604506916051484683>.", color=0xFF93F0)
         embed2 = discord.Embed(color=0xFF93F0)
         await message.channel.send(embed=embed1)
-    if(message.content == "!verification" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
+    elif(message.content == "!verification" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
         embed1 = discord.Embed(title="**༚ ✧˳⁺ Verification ⁺˳✧ ༚**", description="✧ - Post selfie in <#558991375681716256> with 'e- Nightclub and your discord username' on a piece of paper to get verified! \r\n\r\n ✧ - Verified role gives you access to <#558992153393496074> or <#558991846785679360> depends on your gender. \r\n\r\n ✧ - **NOTE:** You cannot use a photo that you took for another server. \r\n\r\n You can also Dm me or dm a staff member to role you verified if you don't want to post your face in <#604506916051484683>.", color=0xFF93F0)
         embed2 = discord.Embed(color=0xFF93F0)
         await message.channel.send(embed=embed1)
-    if(message.content == "!verified" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
+    elif(message.content == "!verified" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
         embed1 = discord.Embed(title="*New member is verified!", description="༚ ✧˳⁺ Member has recieived verified role. ⁺˳✧ ༚ ", color=0xFF93F0)
         embed2 = discord.Embed(color=0xFF93F0)
         await message.channel.send(embed=embed1)
-    if(message.content == "!rewards" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
+    elif(message.content == "!rewards" and (message.author.id == 393839495859929089 or message.author.id == 495680416422821888)):
         embed1 = discord.Embed(title="", description="**──────────» ✰║ Invite Rewards ║✰ «──────────** \r\n\r\n - | ``✰`` | - **__5__ invites:** Pic perms or Respected role (you can only pick one role.) \r\n\r\n - | ``✰`` | - **__10__ invites:** Custom role with color! (last 30 days.) \r\n\r\n - | ``✰`` | - **__20__ invites:** Custom role with private channel (can give anyone access to it.) \r\n\r\n - | ``✰`` | - **__50__ invites:** Classic nitro (5$). \r\n\r\n - | ``✰`` | - **__75__ invites:** Server promo with ping ``@everyone`` \r\n\r\n **─────────────» ✰║ Note: ║✰ «─────────────** \r\n\r\n **➜** Your level must be at least **5+** to get your prize. \r\n\r\n **➜** Make sure to make your **OWN** permanent instant invite. \r\n\r\n **➜** Do NOT make alts to join the server with your invite code. \r\n\r\n **➜** After receiving your prize we'll reset your invites back to **0**. \r\n\r\n **➜** Type **>invites** in <#559063589114216470> to check how many people you have invited. \r\n\r\n Dm <@495680416422821888> if you have any questions.", color=0xFF93F0)
         embed2 = discord.Embed(color=0xFF93F0)
         await message.channel.send(embed=embed1)
-    if message.content == "invite":
+    elif message.content == "invite":
         online = 0
         for member in message.guild.members:
             if member.status != discord.Status.offline:
@@ -158,15 +158,15 @@ async def on_message(message: Message):
 
   #  if "xxx" in message.content:
      #   await message.author.send("Hi")
-    if shiki in message.mentions:
+    elif shiki in message.mentions:
         await message.author.send(f"Hey there, {message.author.mention}! \nPlease don't abusively mention the Devs without a reason. If you want to just talk to them, it's okay, but don't don it oftenly without a real reason. But while you're here... \n\n Are you looking for **cheap** and sometimes **free** __bot developing and hosting__? Our **custom bot**, <@593090256560193549> was made by the user you just pinged, {shiki.mention}. \n\n If you're interesting in having a custom bot like this one, **DM {shiki.mention}** and we'll talk about it there. \n\n > This automatic action was fired because you pinged either the Bot Coder role or {shiki.mention}.")
-    if message.content == "dy":
+    elif message.content == "dy":
         await message.channel.send("dy >_<")
-    if message.content == "lumen":
+    elif message.content == "lumen":
         await message.channel.send("LUMEN XD")
-    if message.content == "shiki":
+    elif message.content == "shiki":
         await message.channel.send(">.<")
-    if message.content == "no u":
+    elif message.content == "no u":
         await message.channel.send("no u")
 
     await bot.process_commands(message)
